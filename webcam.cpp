876d232d79465a557e6a36bbf77cb976df1534aa
pedroManuelP <chamorropedropereira@gmail.com>
2026-08-24 14:05:36 -0300

    second local commit, baby!!!

--- webcam.cpp
+++ webcam.cpp
@@ -3,41 +3,46 @@
 #include <string>
 
 using namespace std;
+using namespace cv;
 
 int main(int arcg, char** arcgv){
-    cv::VideoCapture cap;   // Classe para tratar com captura de vídeo em arquivos e dispositivos
+    VideoCapture cap;   // Classe para tratar com captura de vídeo em arquivos e dispositivos
     double width, height;
-    cv::Mat frame;
+    Mat frame;
     int counter;
 
-    cap.open(0);
-    if(!cap.isOpened()) return -1;
-
-    width = cap.get(cv::CAP_PROP_FRAME_WIDTH); // Duas características do objeto que são as dimensões de 1 frame do vídeo
-    height = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+    cap.open(0, CAP_DSHOW); // CAP_DSHOW é a especificação de API de backend para video. Pode ser usado para forçar uma implementação de leitura específica.
+    if(!cap.isOpened()){
+        cerr <<  "Não foi possível abrir a câmera." << endl;
+        return -1;
+    }
+    width = cap.get(CAP_PROP_FRAME_WIDTH); // Duas características do objeto que são as dimensões de 1 frame do vídeo
+    height = cap.get(CAP_PROP_FRAME_HEIGHT);
     cout << "Largura: " << width << endl;
     cout << "Altura: " << height << endl;
 
-    cv::Size frameSize(static_cast<int>(width), static_cast<int>(height));  // Classe que define o tamanho de um frame do vídeo. Argumento de VideoWriter
+    Size frameSize(static_cast<int>(width), static_cast<int>(height));  // Classe que define o tamanho de um frame do vídeo. Argumento de VideoWriter
 
-    int type = cap.get(cv::CAP_PROP_FOURCC);    // Código(codec) do vídeo, que define o formato de compressão do vídeo. Argumento de VideoWriter
+    int type = VideoWriter::fourcc('M', 'J', 'P', 'G');
+    double fps = cap.get(CAP_PROP_FPS);
+    if (fps <= 0) fps = 30.0;
 
     string path = "../output.avi";  // Destino dos frames do video
-    cv::VideoWriter out (path, type, cap.get(cv::CAP_PROP_FPS), frameSize, true); // o último argumento define se o vídeo será em cores ou não
+    VideoWriter out (path, type, fps, frameSize, true); // o último argumento define se o vídeo será em cores ou não
     for(counter=0; cap.read(frame); counter++){
-        cv::flip(frame, frame, 1);
+        flip(frame, frame, 1);
         out << frame; // Passa o frame lido para o vídeo de saída(VideoWriter)
-        cv::imshow("Frame", frame);
+        imshow("Frame", frame);
         
-        int tecla = cv::waitKey(30);
+        int tecla = waitKey(30);
         if(tecla >= 0) break; // Se alguma tecla foi pressionada, sai do loop
 
-        if(cv::getWindowProperty("Frame", cv::WND_PROP_VISIBLE) < 1) break;
+        if(getWindowProperty("Frame", WND_PROP_VISIBLE) < 1) break;
     }
     std::cout << "Numero de frames: " << counter << "\n";
 
     cap.release(); 
     out.release();
-    cv::destroyAllWindows();
+    destroyAllWindows();
     return 0;
 }
